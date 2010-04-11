7c7b378ba2e3ce1ed637f46eea0e74d91d531514
ratul.mukh <ratul.mukh@830311b4-703b-0410-b43d-057cfd0b486a>
2010-04-11 17:28:41 +0000

    added IP related data structures
    git-svn-id: https://tashjik.googlecode.com/svn/trunk@143 830311b4-703b-0410-b43d-057cfd0b486a

---
+++ Tier2Chord/ChordCommon/IP_Object.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Tashjik.Tier2.ChordCommon
+{
+    public class IP_Object
+    {
+        public IPAddress IP;
+		public Object obj;
+	}
+    
+}
+
