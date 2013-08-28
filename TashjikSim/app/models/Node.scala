61854ec9f76d27a0fcaf218003452fe841ac8b8d
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2013-08-28 05:11:14 -0700

    added pred succ processing

--- TashjikSim/app/models/Node.scala
+++ TashjikSim/app/models/Node.scala
@@ -4,6 +4,7 @@ import play.api._
 import akka.actor.{Actor, ActorRef, Props}
 import akka.event.Logging
 import scala.concurrent.duration._
+import akka.util.Timeout
 import akka.pattern.ask
 import scala.concurrent.Await
 import java.util.UUID
@@ -13,6 +14,8 @@ case class GetPredecessor()
 case class GetSuccessor()
 case class GetSuccessorOfId(id: String)
 case class GetId()
+case class SetNewSuccessor(successor: NodeRep)
+case class SetNewPredecessor(predecessor: NodeRep)
 
 case class NodeRep(node: ActorRef, id: String)
 
@@ -26,13 +29,16 @@ class Node(bootstrapNode: Option[NodeRep]) extends Actor {
   
   bootstrapNode match {
     case None => Logger.info("No bootstrap node available")
-    case Some(value) => {
-       val future1 = value.node.ask(GetPredecessor())(5 seconds)
-       predecessor = Await.result(future1, (5 seconds)).asInstanceOf[NodeRep]
+    case Some(bootstrapNode) => {
        
+       implicit val timeout = Timeout(5 seconds)
        
-       val future2 = value.node.ask(GetSuccessor())(5 seconds)
-       successor = Await.result(future1, (5 seconds)).asInstanceOf[NodeRep]
+       successor =   Await.result((bootstrapNode.node ? GetSuccessorOfId(id)), (5 seconds)).asInstanceOf[NodeRep]
+       predecessor = Await.result((successor.node     ? GetPredecessor()),     (5 seconds)).asInstanceOf[NodeRep]
+       
+       successor.node   ! SetNewPredecessor(NodeRep(context.self, id))
+       predecessor.node ! SetNewSuccessor(NodeRep(context.self, id))
+           
     }
   }
  
@@ -50,16 +56,38 @@ class Node(bootstrapNode: Option[NodeRep]) extends Actor {
     case GetSuccessor() => {
       sender ! successor
     }
+    case SetNewSuccessor(successor: NodeRep) => {
+      this.successor = successor 
+    }
+    
+    case SetNewPredecessor(predecessor: NodeRep) => {
+      this.predecessor = predecessor  
+    }
     
     case GetSuccessorOfId(id) => {
       Logger.info("Query id = " + id )
       Logger.info("Successor id = " + successor.id)
-      if(id.compareTo(successor.id) < 0) {
-        Logger.info("Query id < successor.id")}
+      
+      var querySuccessor: NodeRep = NodeRep(context.self, id)
+      
+      if( id.compareTo(predecessor.id) == 0 || id.compareTo(successor.id) == 0 ) 
+        Logger.warn("id matches predecessor or successor")
+      if( id.compareTo(this.id) > 0 && id.compareTo(successor.id) <= 0 ) {
+        Logger.info("Query success")
+        querySuccessor = NodeRep(context.self, id)
+      }
       else if(id.compareTo(successor.id) > 0) {
-        Logger.info("Query id > successor.id")}
-      else if(id.compareTo(successor.id) == 0) {
-        Logger.info("Query id == successor.id")}
+        implicit val timeout = Timeout(5 seconds)
+        Logger.info("Query needs to be relayed: id > successor.id")
+        querySuccessor = Await.result((successor.node ? GetSuccessorOfId(id)), (5 seconds)).asInstanceOf[NodeRep]
+      }
+      else if(id.compareTo(this.id) < 0) {
+        implicit val timeout = Timeout(5 seconds)
+        Logger.info("Query needs to be relayed: id < successor.id")
+        querySuccessor = Await.result((predecessor.node ? GetSuccessorOfId(id)), (5 seconds)).asInstanceOf[NodeRep]
+      }
+      
+      
         
       
       
--- TashjikSim/app/models/NodeManager.scala
+++ TashjikSim/app/models/NodeManager.scala
@@ -5,10 +5,13 @@ import play.libs.Akka
 import akka.actor.{Actor, ActorRef, Props}
 import akka.event.Logging
 import scala.concurrent.duration._
+import akka.util.Timeout
 import akka.pattern.ask
 import scala.concurrent.Await
 
 case class StartSimulation(nodeCount: Int)
+
+
 
 class NodeManager extends Actor {
   
@@ -21,19 +24,20 @@ class NodeManager extends Actor {
     case StartSimulation(nodeCount: Int) => { 
       Logger.info("Received new simulation request: Node count = " + nodeCount)
       
-      var bootstrapNode= None : Option[ActorRef]
+      var bootstrapNode= None : Option[NodeRep]
       
       for(a <- 1 to nodeCount)
       {
+        implicit val timeout = Timeout(5 seconds)
         //val t: Iterable[ActorRef] = context.children
           val node: ActorRef = context.actorOf(Props(new Node(bootstrapNode)), name = "Node"+a ) 
-        	val future = node.ask("test")(5 seconds)
+        	val future = node ? "test"
         	val result = Await.result(future, (5 seconds)).asInstanceOf[String]
         	Logger.info("returned after testing child with status = " + result)
         	
-        	bootstrapNode = Some(node)
+        	bootstrapNode = Some(NodeRep(node, Await.result(node.ask(GetId())(5 seconds), (5 seconds)).asInstanceOf[String]))
     	  
-      } 
+      }
     }  
     case _      => Logger.info("received unknown message")
   }
