ee9c676034ec9c9b3530437b60c60ee20cd65a92
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2014-05-16 00:07:36 -0600

    tashjiksim versioning

--- TashjikSim/project/Build.scala
+++ TashjikSim/project/Build.scala
@@ -6,8 +6,17 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "TashjikSim"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = System.getProperty("version")
 
+  val playCommonSettings = Seq(
+    organization := "tashjik",
+    crossPaths := false,
+    version := System.getProperty("version"),
+    scalaVersion := "2.10.3",
+    artifactName := { (sv: ScalaVersion, module: ModuleID, artifact: Artifact) =>
+       artifact.name + "_" + System.getProperty("version") + "." + artifact.extension
+    })
+    
     
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -17,16 +26,18 @@ object ApplicationBuild extends Build {
 	"junit" % "junit" % "4.8.1" % "test",
 	"com.typesafe.akka" %% "akka-actor"    % "2.2.3",
 	"com.typesafe.akka" %% "akka-testkit"  % "2.2.3" % "test",
-	"org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+	"org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+	"tashjik" % "tashjikcore" % System.getProperty("version")
 	//"tindr" % "play2pusher_2.9.1" % "1.0.1" // exclude("play", "play_2.9.1")
 	
   )
 
 
   //val main = play.Project(appName, appVersion, appDependencies).settings(atmosPlaySettings: _*).settings(
-    val main = play.Project(appName, appVersion, appDependencies).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(playCommonSettings: _*).settings(
     // Add your own project settings here 
 	
+   
 	resolvers += Resolver.url("Tindr's Play module repository",
 	    url("http://tindr.github.com/releases/"))
 	    (Resolver.ivyStylePatterns)
