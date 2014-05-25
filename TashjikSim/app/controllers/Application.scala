97f8cfc9ce38aebf1bf6e07f4619cdf65b1f1799
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2014-05-25 11:19:45 -0600

    websocket basic

--- TashjikSim/app/controllers/Application.scala
+++ TashjikSim/app/controllers/Application.scala
@@ -5,6 +5,9 @@ import play.api.mvc._
 import tashjik.chord._
 import play.api.libs.concurrent.Execution.Implicits._
 import akka.actor.actorRef2Scala
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.concurrent.Promise
 
 object Application extends Controller {
   
@@ -24,6 +27,22 @@ object Application extends Controller {
     _root_.globals.nodeManager ! StartSimulation(nodeCount, dataObjectsCount)
     
     Ok("Simulation request received for " + nodeCount + " node")
+  }
+  
+  
+  def dynamicCount = Action {
+    Ok.chunked(Enumerator("2"))
+  }
+  
+  def websocket = WebSocket.using[String] { request => 
+  
+  	// Just consume and ignore the input
+  	val in = Iteratee.consume[String]()
+  
+  	// Send a single 'Hello!' message and close
+  	//val out = Enumerator("Hello!") >>> Enumerator.eof
+    val out = Enumerator.generateM(Promise.timeout(Some("Hello!"), 500)) // >>> Enumerator.eof
+  	(in, out)
   }
 }
 
