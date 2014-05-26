2659c1494a89e6916b379dcdbda2b57a8cf7f6cc
Ratul Mukhopadhyay <ratul.mukh@gmail.com>
2014-05-26 14:38:24 -0600

    websocket on formload 1

--- TashjikSim/app/controllers/Application.scala
+++ TashjikSim/app/controllers/Application.scala
@@ -8,12 +8,14 @@ import akka.actor.actorRef2Scala
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.iteratee.Iteratee
 import play.api.libs.concurrent.Promise
+import play.api.libs.iteratee.Concurrent
 
 object Application extends Controller {
   
+    
   def index = Action {
     val version = Play.current.configuration.getString("tashjik.version")
-    Ok(views.html.index(20, "Logs"))
+    Ok(views.html.index0(20, "Logs"))
     //Ok(views.html.main("Tashjik", version))
   }
 
@@ -21,12 +23,12 @@ object Application extends Controller {
     Logger.info("Received request body = " +request.body)
     
       
-    
+      
     val nodeCount = request.body.asFormUrlEncoded.get("nodeCount")(0).toInt
     val dataObjectsCount = request.body.asFormUrlEncoded.get("dataObjectsCount")(0).toInt
     _root_.globals.nodeManager ! StartSimulation(nodeCount, dataObjectsCount)
     
-    Ok("Simulation request received for " + nodeCount + " node")
+    Ok(views.html.index(20, "Logs"))
   }
   
   
@@ -37,11 +39,30 @@ object Application extends Controller {
   def websocket = WebSocket.using[String] { request => 
   
   	// Just consume and ignore the input
-  	val in = Iteratee.consume[String]()
+  	//val in = Iteratee.consume[String]()
   
   	// Send a single 'Hello!' message and close
-  	//val out = Enumerator("Hello!") >>> Enumerator.eof
-    val out = Enumerator.generateM(Promise.timeout(Some("Hello!"), 500)) // >>> Enumerator.eof
+  	//val out = Enumerator("""{"result":true,"count":1}""") // >>> Enumerator.eof
+  	//			<circle cx="290" cy="290" r="225" fill="purple" />
+		//	<circle cx="290" cy="290" r="221" fill="white" />
+			//<!--  -->circle cx="483.12" cy="401.5" r="10" fill="red" --/>
+  	
+  	//val out = Enumerator("""{"svgType":"circle", "cx":290, "cy": 290, "r":225, "fill": "purple"}""")
+  	
+  	//out >>> Enumerator("""{"svgType":"rectangle", "cx":290, "cy": 290, "r":225, "fill": "purple"}""")
+  	
+  	//enume = out
+    //val out = Enumerator.generateM(Promise.timeout(Some("Hello!"), 500)) // >>> Enumerator.eof
+    
+    //Concurrent.broadcast returns (Enumerator, Concurrent.Channel)
+    val (out,channel) = Concurrent.broadcast[String]
+    //log the message to stdout and send response back to client
+    val in = Iteratee.foreach[String] {
+      msg => println(msg)
+             //the Enumerator returned by Concurrent.broadcast subscribes to the channel and will 
+             //receive the pushed messages
+             channel push("RESPONSE: " + msg)
+    }
   	(in, out)
   }
 }
